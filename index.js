36060a2682f7989115882f2b97d502171be493f3
ThiaV <thiacrt@gmail.com>
2023-08-17 15:02:25 +0700

    Change some texts

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const epress = require('express')
+const express = require('express')
 const app = express()
 const port = 3000
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log('Example app listen at http://localhst:${port}'))
+app.listen(port, () => console.log(`Example app listen at http://localhost:${port}`))
